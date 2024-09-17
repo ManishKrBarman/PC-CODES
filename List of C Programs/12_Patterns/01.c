4cec2963e6a9bb26efb87bd6f301113070ee4721
Manish Kumar Barman <manishkumarbarman111@gmail.com>
2024-09-18 00:24:31 +0530

    Add files via upload

---
+++ List of C Programs/12_Patterns/01.c
@@ -0,0 +1,15 @@
+// Author: Manish Kumar Barman
+
+#include <stdio.h>
+
+int main() {
+  int i, j;
+  for (i = 5; i > 0; i--) {
+    for (j = 1; j <= i; j++) {
+      printf("%d ", j);
+    }
+    printf("\n");
+  }
+
+  return 0;
+}
