50cbefcd394f7fb633c201ce4dec9bedb293bed4
Manish Kumar Barman <manishkumarbarman111@gmail.com>
2024-10-23 07:59:13 +0530

    Update 04.c

--- List of C Programs/12_Patterns/04.c
+++ List of C Programs/12_Patterns/04.c
@@ -1,12 +1,14 @@
+
 // Author: Manish Kumar Barman
 
 #include <stdio.h>
 
 int main() {
   int i, j, k;
+  k = 1;
   for (i = 1; i < 5; i++) {
     for (j = 1; j <= i; j++) {
-      printf("%d ", k++);
+        printf("%d ", k++);
     }
     printf("\n");
   }
